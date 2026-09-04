c430d3c84cb241c0d95b03bcd7376506b72dad06
Supersnorlax100 <samuel2122009@gmail.com>
2026-09-04 12:54:39 -0500

    healthbar

--- Assets/Scripts/TargetScript.cs
+++ Assets/Scripts/TargetScript.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetScript : MonoBehaviour
 {
     [SerializeField] int health;
     public int scoreValue;
+    private Slider healthBar;
 
-    public void Start()
+    private void Awake()
     {
-        
+        healthBar = GetComponentInChildren<Slider>();
+        healthBar.maxValue = health;
+        healthBar.value = health;
+        healthBar.gameObject.SetActive(false);
     }
     public void GetHit(int damage)
     {
         health -= damage;
+        UpdateHealthBar();
         if (health <= 0)
         {
             EventHandeler.onTargetDeath?.Invoke();
@@ -23,9 +29,12 @@ public class TargetScript : MonoBehaviour
     {
 
     }
-
-    public void Die()
+    void UpdateHealthBar()
     {
-
+        if (!healthBar.gameObject.activeSelf)
+        {
+            healthBar.gameObject.SetActive(true);
+        }
+        healthBar.value = health;
     }
 }
