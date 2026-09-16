7509e83f8127e7d4bb65baffdf616ea8a05753a7
BobTheSniper301 <owenspeyton159@gmail.com>
2026-09-16 15:00:12 -0500

    still broken fml

--- Assets/Scripts/TargetStuff/TargetSpawnerScript.cs
+++ Assets/Scripts/TargetStuff/TargetSpawnerScript.cs
@@ -40,31 +40,46 @@ public class TargetSpawnerScript : MonoBehaviour
         {
             // The + YtargetSpawnAreaScale/10 is because the target spawns a little too low than what it should so I added an offset
             // The sqrt is to out to if a square was rotated 45 
-            float targetX = targetSpawnArea.transform.position.x + Random.Range(-(XtargetSpawnAreaScale/2 * Mathf.Sqrt(2)) + XtargetSpawnAreaScale/25, XtargetSpawnAreaScale/2 * Mathf.Sqrt(2) - XtargetSpawnAreaScale/25);
-            float targetY = targetSpawnArea.transform.position.y + Random.Range(-(YtargetSpawnAreaScale/2 * Mathf.Sqrt(2)) + YtargetSpawnAreaScale/10, YtargetSpawnAreaScale/2 * Mathf.Sqrt(2) + YtargetSpawnAreaScale/25);
-            float targetZ = targetSpawnArea.transform.position.z + 0.3f; // just added offset
-            Vector3 targetPos = new Vector3(targetX, targetY, targetZ);
+
+            //float targetX = targetSpawnArea.transform.position.x + Random.Range(-(XtargetSpawnAreaScale/2 * Mathf.Sqrt(2)) + XtargetSpawnAreaScale/25, XtargetSpawnAreaScale/2 * Mathf.Sqrt(2) - XtargetSpawnAreaScale/25);
+            //float targetY = targetSpawnArea.transform.position.y + Random.Range(-(YtargetSpawnAreaScale/2 * Mathf.Sqrt(2)) + YtargetSpawnAreaScale/10, YtargetSpawnAreaScale/2 * Mathf.Sqrt(2) + YtargetSpawnAreaScale/25);
+            //float targetZ = targetSpawnArea.transform.position.z + 0.3f; // just added offset
+            float targetX = 0;
+            float targetY = 5;
+            float targetZ = 0.55f;
+            Vector3 targetPos = new Vector3(targetX, targetY, targetZ - transform.position.z);
             Collider[] targetCollisions;
             Collider[] spawnableCollisions;
-            targetCollisions = Physics.OverlapSphere(targetPos, target.GetComponentInChildren<SphereCollider>().radius + targetSpace, layerMask: 6);
-            spawnableCollisions = Physics.OverlapSphere(targetPos, 0.001f, layerMask: 9);
+            //Collider[] Collisions;
+            targetCollisions = Physics.OverlapSphere(targetPos, target.GetComponentInChildren<SphereCollider>().radius + targetSpace);
+            spawnableCollisions = Physics.OverlapSphere(targetPos, 0.1f);
+            //Collisions = Physics.OverlapSphere(targetPos, 0.5f, layerMask: 9);
+            //GameObject _target = Instantiate(target, new Vector3(targetX,targetY,.55f), Quaternion.identity);
+            //_target.GetComponent<Transform>().localScale = new Vector3(.1f,.1f,.1f);
 
             // number after layer is equal to the layer of TargetSpawnable
             //if ((targetCollisions.Length == 1 && (spawnableCollisions.Length == 0 || spawnableCollisions[0].gameObject.layer == 9)) || i == attemptsToSpawn - 1)
 
             //if ((targetCollisions[0].gameObject.layer == 9) || i == attemptsToSpawn - 1)
-            if ((targetCollisions.Length == 0 && spawnableCollisions.Length == 1))
+            //if ((targetCollisions.Length == 0 && spawnableCollisions.Length == 1))
+
+            Debug.Log("length: " + targetCollisions.Length);
+            Debug.Log("target coll: " );
+            foreach (Collider coll in targetCollisions) { Debug.Log("targ coll: " + coll.name); }
+            Debug.Log("spawnable collisions length: " + spawnableCollisions[0].gameObject.layer);
+            Debug.Log("colliders: ");
+            foreach (Collider coll in spawnableCollisions) { Debug.Log("spawn coll: " + coll.name); }
+            if (spawnableCollisions.Length == 1 && spawnableCollisions[0].gameObject.layer == 9 && targetCollisions.Length > 0 && (targetCollisions[0].gameObject.layer == 6 || targetCollisions[1].gameObject.layer == 6))
             {
                 //Debug.Log("layer: " + targetCollisions[0].gameObject.layer + " obj: " + targetCollisions[0].gameObject.name + " length: " + targetCollisions.Length);
-                //Debug.Log("all collisions: " );
-                //Debug.Log(" length: " + targetCollisions.Length);
-                Debug.Log("colliders: ");
-                foreach (Collider coll in spawnableCollisions) { Debug.Log("spawn coll: " + coll.name); }
-                foreach (Collider coll in targetCollisions) { Debug.Log("targ coll: " + coll.name); }
-                if (i == attemptsToSpawn-1)
-                {
-                    Debug.Log("force");
-                }
+                
+                //Debug.Log("Test colliders: ");
+                //foreach (Collider coll in Collisions) { Debug.Log("spawn coll: " + coll.name); }
+                //foreach (Collider coll in targetCollisions) { Debug.Log("targ coll: " + coll.name); }
+                //if (i == attemptsToSpawn-1)
+                //{
+                //    Debug.Log("force");
+                //}
                 GameObject _target = Instantiate(target, targetPos, Quaternion.identity, targetParent.transform);
                 _target = _target.transform.GetChild(0).gameObject;
                 if (areTargetsMoving)
