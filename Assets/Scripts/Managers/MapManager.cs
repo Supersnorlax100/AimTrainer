fc0fd836527702e9c92cc2ba75a757fa7cfab3ed
BobTheSniper301 <owenspeyton159@gmail.com>
2026-09-24 15:43:05 -0500

    Map stuff better
    Can go to scenes and whatnot. Need logic for button availability

--- Assets/Scripts/Managers/MapManager.cs
+++ Assets/Scripts/Managers/MapManager.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public enum RoomType
 {
@@ -15,14 +17,20 @@ public class MapManager : MonoBehaviour
 {
     public static MapManager instance;
 
+    // Room Types
+    // 0 - Home
+    // 1 - Combat
+    // 2 - Shop
+    // 3 - Event
+    // 4 - MiniBoss
+    // 5 - Boss
     private Dictionary<Vector2, RoomType> roomMap = new Dictionary<Vector2, RoomType>();
-    [SerializeField] private List<Sprite> roomSprites = new List<Sprite>();
+    private Dictionary<Vector2, GameObject> nodeMap = new Dictionary<Vector2, GameObject>();
     [SerializeField] private int floorsNumber;
     [SerializeField] private int RoomsPerFloor;
 
-    public int[] rooms;
-    public GameObject[] roomObjs;
     public Sprite[] sprites;
+    [SerializeField] GameObject mapNode;
 
     private void Awake()
     {
@@ -39,20 +47,17 @@ public class MapManager : MonoBehaviour
     private void Start()
     {
         MakeMap();
-        for (int i = 0; i < rooms.Length; i++)
+        foreach (GameObject mapNode in nodeMap.Values)
         {
-            int j = Random.Range(0, 4);
-            rooms[i] = j;
-            roomObjs[i].GetComponent<MapNodeScript>().roomNum = i;
-            roomObjs[i].GetComponent<MapNodeScript>().roomType = j;
-            roomObjs[i].GetComponent<Image>().sprite = sprites[j];
+            Debug.Log(mapNode.name);
         }
     }
 
-    public void GoToRoom(int room)
+    public void GoToRoom(Vector2 room)
     {
         Debug.Log("go to room: " + room);
-        
+        Debug.Log(roomMap[room]);
+        SceneManager.LoadScene((int)roomMap[room]);
     }
 
     private void MakeMap()
@@ -63,19 +68,44 @@ public class MapManager : MonoBehaviour
             for (int room = 0; room < RoomsPerFloor; room++)
             {
                 Vector2 roomPosition = new Vector2(room, floor);
-                int randomRoomTypeIndex = Random.Range(0, System.Enum.GetValues(typeof(RoomType)).Length);
+                // The 1 and -1 are to ignore the home and boss sprites
+                int randomRoomTypeIndex = Random.Range(1, System.Enum.GetValues(typeof(RoomType)).Length -1);
                 RoomType randomRoomType = (RoomType)randomRoomTypeIndex; 
                 roomMap.Add(roomPosition, randomRoomType);
+                GenerateMapNode(roomPosition);
             }
         }
+
+        Vector2 _roomPosition = new Vector2(1, -1);
+        RoomType roomType = RoomType.HOME;
+        roomMap.Add(_roomPosition, roomType);
+        GenerateMapNode(_roomPosition);
+        // Boss Node
+        _roomPosition = new Vector2(1, floorsNumber);
+        roomType = RoomType.BOSS;
+        roomMap.Add(_roomPosition, roomType);
+        GenerateMapNode(_roomPosition);
+    }
+
+    void GenerateMapNode(Vector2 roomPosition)
+    {
+        Vector3 mapNodePos = new Vector3(roomPosition.x * 150 - 150, roomPosition.y * 150 - 300, 0);
+        GameObject _mapNode = Instantiate(mapNode, Vector3.zero, Quaternion.identity, transform);
+        _mapNode.transform.localPosition = mapNodePos;
+        _mapNode.GetComponent<MapNodeScript>().roomNum = roomPosition;
+        nodeMap.Add(roomPosition, _mapNode);
     }
 
     public Sprite GetRoomSprite(Vector2 roomPosition)
     {
-        string roomName = roomMap[roomPosition].ToString().ToLower();
-        Sprite roomSprite = roomSprites.Find(sprite => sprite.name.ToLower() == roomName);
+        Sprite roomSprite = sprites[(int)roomMap[roomPosition]];
+        //string roomName = roomMap[roomPosition].ToString().ToLower();
+        //Sprite roomSprite = roomSprites.Find(sprite => sprite.name.ToLower() == roomName);
         return roomSprite;
     }
 
-
+    public void NodeAvailability()
+    {
+        //#TODO: Determine which buttons are available
+    }
 }
--- Assets/Scripts/MapNodeScript.cs
+++ Assets/Scripts/MapNodeScript.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapNodeScript : MonoBehaviour
 {
     public int roomType;
-    public int roomNum;
+    public Vector2 roomNum;
 
+    public void Start()
+    {
+        GetComponent<Image>().sprite = MapManager.instance.GetRoomSprite(roomNum);
+    }
     public void GoToRoom()
     {
         MapManager.instance?.GoToRoom(roomNum);
